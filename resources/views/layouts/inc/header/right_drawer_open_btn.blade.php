01473db11deb0dd212501a530cf9197c9b0f5d01
Sachee <sachee999@gmail.com>
2020-04-08 12:07:52 +0530

    2nd commit

--- resources/views/layouts/inc/header/right_drawer_open_btn.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="header-btn-lg">
-    <button type="button" class="hamburger hamburger--elastic open-right-drawer">
-        <span class="hamburger-box">
-            <span class="hamburger-inner"></span>
-        </span>
-    </button>
-</div>
